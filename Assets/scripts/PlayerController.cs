393cc4f769880a29c3329a49d276bc3e95915f99
kph <khmlpa@gmail.com>
2016-10-26 17:16:32 -0700

    initial commit rabm

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	private float movehorizontal;
+	private float movevertical;
+	private int count;
+	public Text countText;
+	public Text wintext;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		wintext.text = "";
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+	}
+
+
+	void FixedUpdate()
+	{
+
+		//movehorizontal = Input.GetAxis ("Horizontal");
+		//movevertical = Input.GetAxis ("Vertical");
+		movehorizontal = Input.acceleration.x;
+		movevertical = Input.acceleration.y;
+
+		Vector3 movement = new Vector3 (movehorizontal, 0.0f, movevertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Pick up"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+
+	}
+
+
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if(count >=23){wintext.text = "You Win";
+			SceneManager.LoadScene ("rab_scene");}
+	}
+
+}
