6ef013397318fb51d2627e3a899d113dd5c3a838
kph <khmlpa@gmail.com>
2016-10-26 20:00:14 -0700

    update controls

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	private float mspeed;
+	private float mmovehorizontal;
+	private float mmovevertical;
 	private float movehorizontal;
 	private float movevertical;
 	private int count;
@@ -16,6 +19,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		rb = GetComponent<Rigidbody>();
 		count = 0;
+		mspeed = speed / 5;
 		SetCountText ();
 		wintext.text = "";
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
@@ -25,14 +29,16 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-
-		//movehorizontal = Input.GetAxis ("Horizontal");
-		//movevertical = Input.GetAxis ("Vertical");
+		mmovehorizontal = Input.GetAxis ("Horizontal");
+		mmovevertical = Input.GetAxis ("Vertical");
 		movehorizontal = Input.acceleration.x;
 		movevertical = Input.acceleration.y;
 
 		Vector3 movement = new Vector3 (movehorizontal, 0.0f, movevertical);
 		rb.AddForce (movement * speed);
+
+		Vector3 mmovement = new Vector3 (mmovehorizontal, 0.0f, mmovevertical);
+		rb.AddForce (mmovement * mspeed);
 	}
 
 	void OnTriggerEnter(Collider other)
